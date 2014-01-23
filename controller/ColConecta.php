08429a2fef269c3cb5fb9c28a8e66caf2c11a8d8
Maison Kendi Sakamoto <maison.sakamoto@gmail.com>
2014-01-22 18:48:35 -0800

    commit maison

--- controller/ColConecta.php
+++ controller/ColConecta.php
@@ -12,7 +12,7 @@ class ColConecta {
     }
 
     public function conectar() {
-        @mysql_connect('127.0.0.1', '', '');
+        @mysql_connect('127.0.0.1', 'root', '');
         @mysql_select_db('testes');
     }
 
